1f7b97dbb3138599d10fe81320e8e65d783ac0b9
Andrey Germanov <andrey@germanov.dev>
2023-04-23 13:24:19 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# yolov8_pytorch_python
-YOLOv8 inference using Ultralytics API
+# YOLOv8 inference using Ultralytics API
+
+This is a source code of a web application for this article: 
+https://dev.to/andreygermanov/create-an-object-detector-using-yolov8-neural-network-and-deploy-it-as-a-web-service-on-python-julia-nodejs-and-javascript-39fn
+
+This is a web interface to [YOLOv8 object detection neural network](https://ultralytics.com/yolov8) 
+implemented on [Python](https://www.python.org) that uses a model to detect traffic lights on images.
+
+## Install
+
+* Clone this repository: `git clone git@github.com:AndreyGermanov/yolov8_pytorch_python.git`
+* Go to the root of cloned repository
+* Install dependencies by running `pip3 install -r requirements.txt`
+
+## Run
+
+Execute:
+
+```
+python3 object_detector.py
+```
+
+It will start a webserver on http://localhost:8080. Use any web browser to open the web interface.
+
+Using the interface you can upload the image to the object detector and see bounding boxes of all objects detected on it.
---
+++ object_detector.py
@@ -0,0 +1,54 @@
+from ultralytics import YOLO
+import json
+from flask import request, Response, Flask
+from waitress import serve
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    """
+    Site main page handler function.
+    :return: Content of index.html file
+    """
+    with open("index.html") as file:
+        return file.read()
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    """
+        Handler of /detect POST endpoint
+        Receives uploaded file with a name "image_file", passes it
+        through YOLOv8 object detection network and returns and array
+        of bounding boxes.
+        :return: a JSON array of objects bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    buf = request.files["image_file"]
+    boxes = detect_objects_on_image(Image.open(buf.stream))
+    return Response(json.dumps(boxes),  mimetype='application/json')
+
+
+def detect_objects_on_image(buf):
+    """
+    Function receives an image,
+    passes it through YOLOv8 neural network
+    and returns an array of detected objects
+    and their bounding boxes
+    :param buf: Input image file stream
+    :return: Array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    model = YOLO("best.pt")
+    results = model(buf)
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = box.xyxy[0].tolist()
+        class_id = box.cls[0].item()
+        prob = box.conf[0].item()
+        output.append([x1, y1, x2, y2, result.names[class_id], prob])
+    return output
+
+
+serve(app, host='0.0.0.0', port=8080)
