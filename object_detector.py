62fc29bccb3eddab92037e562447d7edbf7a97ae
Andrey Germanov <andrey@germanov.dev>
2023-04-25 19:05:33 +0300

    Code update

--- object_detector.py
+++ object_detector.py
@@ -44,10 +44,14 @@ def detect_objects_on_image(buf):
     result = results[0]
     output = []
     for box in result.boxes:
-        x1, y1, x2, y2 = box.xyxy[0].tolist()
+        x1, y1, x2, y2 = [
+            round(x) for x in box.xyxy[0].tolist()
+        ]
         class_id = box.cls[0].item()
-        prob = box.conf[0].item()
-        output.append([x1, y1, x2, y2, result.names[class_id], prob])
+        prob = round(box.conf[0].item(), 2)
+        output.append([
+            x1, y1, x2, y2, result.names[class_id], prob
+        ])
     return output
 
 
